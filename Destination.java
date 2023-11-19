0a380d4f692ee35b12d6b0736198eb32e3078cce
Raizo-03 <123054277+Raizo-03@users.noreply.github.com>
2023-11-19 22:52:07 +0800

    Add files via upload

---
+++ Destination.java
@@ -0,0 +1,20 @@
+package Main;
+
+public class Destination {
+	private String destination;
+	
+	
+	public Destination(String destination) {
+		this.destination = destination;
+	}
+	public Destination() {
+	}
+	
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	
+	public String getDestination() {
+		return destination;
+	}
+}
