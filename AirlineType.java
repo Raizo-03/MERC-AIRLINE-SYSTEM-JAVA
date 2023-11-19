d1be9468a82ac027a968c78440d7f538c5af1242
Raizo-03 <123054277+Raizo-03@users.noreply.github.com>
2023-11-19 22:50:43 +0800

    Add files via upload

---
+++ AirlineType.java
@@ -0,0 +1,21 @@
+package Main;
+
+public class AirlineType {
+ 	private String airlineT;
+ 	
+ 	
+ 	public AirlineType(String airlineT) {
+ 		this.airlineT = airlineT;
+ 	}
+ 	public AirlineType() {
+ 	
+ 	}
+ 	
+ 	public void setAirlineT(String airlineT) {
+ 		this.airlineT = airlineT;
+ 	}
+ 	
+ 	public String getAirlineT() {
+ 		return airlineT;
+ 	}
+}
